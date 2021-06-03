cd3ede51435d23222b5ae830089f132cdfe8e415
itcolors <caoxingchi@outlook.com>
2021-06-03 23:39:35 +0800

    first commit android

---
+++ app/src/main/java/site/xingchi/MainActivity.java
@@ -0,0 +1,38 @@
+package site.xingchi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        initData();
+    }
+
+    public void initView(){
+        btn=findViewById(R.id.btn);
+    }
+
+    public void initData(){
+        btn.setOnClickListener(new BtnListener());
+    }
+
+    class BtnListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            if(v==btn){
+                Toast.makeText(getApplicationContext(),"点击到我了",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
